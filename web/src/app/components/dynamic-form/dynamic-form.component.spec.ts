223f3d46aa07d05f0edf642dc910d447ef801a41
alejandro <ale.luis@gmail.com>
2024-05-09 18:56:17 -0300

    add jest for angular

--- web/src/app/components/dynamic-form/dynamic-form.component.spec.ts
+++ web/src/app/components/dynamic-form/dynamic-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DynamicFormComponent } from './dynamic-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DynamicFormComponent', () => {
   let component: DynamicFormComponent;
@@ -8,11 +8,61 @@ describe('DynamicFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DynamicFormComponent]
+      imports: [DynamicFormComponent, HttpClientTestingModule]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(DynamicFormComponent);
+    fixture.componentRef.setInput("formStructure", 
+    [
+      {
+        type: "text",
+        label: "Name",
+        name: "name",
+        value: "",
+        validations: [
+          {
+            validator: "required",
+            message: "Name is required"
+          }
+        ]
+      },
+      {
+        type: "email",
+        label: "Email",
+        name: "email",
+        value: "",
+        validations: [
+          {
+            validator: "required",
+            message: "Email is required"
+          },
+          {
+            validator: "email",
+            message: "Invalid email format"
+          }
+        ]
+      },
+      {
+        type: "date",
+        label: "Date",
+        name: "date",
+        value: "",
+        validations: [
+          {
+            validator: "required",
+            message: "Date is required"
+          }
+        ]
+      }
+    ]);
+
+    fixture.componentRef.setInput("initialData",
+    {
+      name: "prueba", 
+      email: "prueba@prueba",
+      date: "2020-01-01"
+    });
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +70,18 @@ describe('DynamicFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate the text input field', () => {
+    expect(component.dynamicForm.get('name')?.valid).toBeTruthy();
+  });
+
+  it('should validate the email input field', () => {
+    expect(component.dynamicForm.get('email')?.valid).toBeTruthy();
+  });
+
+  it('should validate the date input field', () => {
+    expect(component.dynamicForm.get('date')?.valid).toBeTruthy();
+  });
+
+
 });
